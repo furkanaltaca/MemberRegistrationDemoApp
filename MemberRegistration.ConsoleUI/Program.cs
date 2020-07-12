f8349d67a4eeda75888cb7eb6ade47ed88885851
furkanaltaca <furkanaltaca@gmail.com>
2020-07-12 17:10:04 +0300

    Dependency Resolvers added. Business tests added. ConsoleUI implementations done.

--- MemberRegistration.ConsoleUI/Program.cs
+++ MemberRegistration.ConsoleUI/Program.cs
@@ -1,6 +1,9 @@
 ﻿using MemberRegistration.Business.Abstract;
 using MemberRegistration.Business.Concrete.Managers;
+using MemberRegistration.Business.DependencyResolvers.Ninject;
+using MemberRegistration.Business.ServiceAdapters.KpsService;
 using MemberRegistration.DataAccess.Concrete.EntityFramework;
+using MemberRegistration.Entities.Concrete;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,11 +16,29 @@ namespace MemberRegistration.ConsoleUI
     {
         static void Main(string[] args)
         {
-            IMemberService _memberService = new MemberManager(new EfMemberDal());
-            var members = _memberService.GetAll();
-            foreach (var member in members)
+            IMemberService _memberService = InstanceFactory.GetInstance<IMemberService>();
+
+            try
             {
-                Console.WriteLine(member.FirstName);
+                _memberService.Add(new Member
+                {
+                    FirstName = "Furkan",
+                    LastName = "Altaca",
+                    DateOfBirth = new DateTime(2001, 6, 16),
+                    TcNo = "46522310036",
+                    Email = "deneme@gmail.com",
+                });
+                Console.WriteLine("Eklendi");
+
+                var members = _memberService.GetAll();
+                foreach (var member in members)
+                {
+                    Console.WriteLine(member.FirstName);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
             }
             Console.Read();
         }
