219d7432ec3d6f6b4fbb9245bd2add2f39ebc7dc
furkanaltaca <furkanaltaca@gmail.com>
2020-07-12 15:44:58 +0300

    Business tests created. Member validator added.

---
+++ MemberRegistration.Business.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("MemberRegistration.Business.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MemberRegistration.Business.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("99e204ff-0b67-4591-8ecd-606b767a73f2")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ MemberRegistration.Business/ValidationRules/FluentValidation/MemberValidator.cs
@@ -0,0 +1,25 @@
+﻿using FluentValidation;
+using MemberRegistration.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MemberRegistration.Business.ValidationRules.FluentValidation
+{
+    public class MemberValidator : AbstractValidator<Member>
+    {
+        public MemberValidator()
+        {
+            RuleFor(x => x.FirstName).NotEmpty();
+            RuleFor(x => x.LastName).NotEmpty();
+            RuleFor(x => x.DateOfBirth).NotEmpty();
+            RuleFor(x => x.TcNo).NotEmpty();
+            RuleFor(x => x.Email).NotEmpty();
+
+            RuleFor(x => x.DateOfBirth).LessThan(DateTime.Now);
+
+        }
+    }
+}
